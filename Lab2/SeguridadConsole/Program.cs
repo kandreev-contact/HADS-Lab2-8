b5b35171f89a6545e4d8ebb40660c4099314bfab
kandreev-contact <68382841+kandreev-contact@users.noreply.github.com>
2022-03-28 23:02:47 +0200

    Branch lab06-seguridad added

---
+++ Lab2/SeguridadConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography; 
+
+namespace SeguridadConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Simulacion login hashing");
+            Console.ReadLine();
+
+            // Introducir pass-to-hash
+
+            // Check the pass wiht the hashing algo
+
+            // Gain access
+
+            MD5CryptoServiceProvider mD5 = new MD5CryptoServiceProvider();
+            mD5.Initialize();
+        }
+    }
+}

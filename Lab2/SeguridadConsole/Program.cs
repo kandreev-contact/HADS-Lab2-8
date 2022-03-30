29b8bb02204ddff66d5130b7ffd4eb30a5b46a87
kandreev-contact <68382841+kandreev-contact@users.noreply.github.com>
2022-03-30 13:16:23 +0200

    Encripcion de contraseña añadida
    Encripcion añadida en login.

--- Lab2/SeguridadConsole/Program.cs
+++ Lab2/SeguridadConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Security.Cryptography; 
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SeguridadConsole
 {
@@ -11,13 +12,50 @@ namespace SeguridadConsole
             Console.ReadLine();
 
             // Introducir pass-to-hash
+            String to_hash = "blanco";
+
+            String result = Md5(to_hash);
+
+            String answer = "6403558c0e945a72056d09ac3281f964";
+
+            Console.WriteLine("### Dato a hashear: ");
+            Console.WriteLine("### toHash " + to_hash);
+            Console.WriteLine("### Answer " + answer);
+            Console.WriteLine("### Resultado " + result);
 
             // Check the pass wiht the hashing algo
+            if (answer.Equals(result))
+            {
+                Console.WriteLine("### CORRECT!");
+            }
+            else
+            {
+                Console.WriteLine("### INCORRECT!");
+            }
 
             // Gain access
+            Console.ReadLine();
 
-            MD5CryptoServiceProvider mD5 = new MD5CryptoServiceProvider();
-            mD5.Initialize();
+        }
+
+        /// <summary>
+        /// Funcion hash obtenida de, https://developpaper.com/encryption-algorithms-md5-sha1/
+        /// </summary>
+        /// <param name="str"> String to be hashed</param>
+        /// <returns>Returns the hashed string</returns>
+        public static string Md5(string str)
+        {
+            var buffer = Encoding.UTF8.GetBytes(str);
+
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = md5.ComputeHash(buffer);
+
+            var sb = new StringBuilder();
+            foreach (var t in bytes)
+            {
+                sb.Append(t.ToString("X2"));
+            }
+            return sb.ToString().ToLower();
         }
     }
 }
